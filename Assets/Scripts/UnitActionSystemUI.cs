c1596b76b9eef828b8dd254f800fc0acee06b50f
MaximusDD2002 <2maxim2@mail.ru>
2024-01-27 19:13:24 +0100

    Updated the unit action button UI
    Updated the unit action button UI so as it will show to what action the button belongs

--- Assets/Scripts/UnitActionSystemUI.cs
+++ Assets/Scripts/UnitActionSystemUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,26 @@ public class UnitActionSystemUI : MonoBehaviour
     [SerializeField] private Transform actionButtonContainerTransform;
     private void Start()
     {
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
         CreateUnitActionButtons();
     }
     private void CreateUnitActionButtons()
     {
+        foreach(Transform buttonTransform in actionButtonContainerTransform)
+        {
+            Destroy(buttonTransform.gameObject);
+        }
         Unit selectedUnit = UnitActionSystem.Instance.GetSelectedUnit();
 
         foreach (BaseAction baseAction in selectedUnit.GetBaseActionArray())
         {
-            Instantiate(actionButtonPrefab, actionButtonContainerTransform);
+            Transform actionButtonTransform = Instantiate(actionButtonPrefab, actionButtonContainerTransform);
+            ActionButtonUI actionButtonUI =actionButtonTransform.GetComponent<ActionButtonUI>();
+            actionButtonUI.SetBaseAction(baseAction);
         }
+    }
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs e)
+    {
+        CreateUnitActionButtons();
     }
 }
